bb5df3b92422abefec62465dd0e50842ec753b73
Kaja Ali Mohiddin Sheik <ksheik@unomaha.edu>
2023-10-23 21:26:43 -0500

    Initial commit

---
+++ CSCI4650850FinalProject/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for CSCI4650850FinalProject project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.2/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'CSCI4650850FinalProject.settings')
+
+application = get_asgi_application()
---
+++ CSCI4650850FinalProjectapp/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class Csci4650850FinalprojectappConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'CSCI4650850FinalProjectapp'
