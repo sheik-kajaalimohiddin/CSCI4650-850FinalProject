ad87427c6306012a640d197499e619e294cbbe5c
Kaja Ali Mohiddin Sheik <ksheik@unomaha.edu>
2023-11-09 22:30:00 -0600

    adding db creds in settings.py

--- CSCI4650850FinalProject/settings.py
+++ CSCI4650850FinalProject/settings.py
@@ -79,13 +79,22 @@ WSGI_APPLICATION = 'CSCI4650850FinalProject.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'testDB',
+        'USER': 'admin',
+        'PASSWORD': 'admin1234',
+        'HOST': 'database-2.c64umoefw3nt.us-east-1.rds.amazonaws.com',  # Or an IP Address that your DB is hosted on
+        'PORT': '3306',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
